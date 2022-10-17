d726900286b5f1b4ecc5e194502b89e6d3964503
dqmis <dom.seputis@gmail.com>
2022-10-17 19:08:39 +0300

    Added new file

---
+++ src/calculator.py
@@ -0,0 +1,17 @@
+class Calculator:
+    def __init__(self) -> None:
+
+        print("labas")
+
+
+def sum(a: int, b: int) -> int:
+
+    return a + b
+
+
+if __name__ == "__main__":
+    kintamas = "zodis"
+    kintamas = str(sum(10, 10))
+    kintamas1 = sum(10, 10)
+
+    c = Calculator()
