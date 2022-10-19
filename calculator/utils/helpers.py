56a98eb7a247206bc82d14b08bc9aeb150bc2beb
dqmis <dom.seputis@gmail.com>
2022-10-19 21:27:55 +0300

    Added decorators

--- calculator/utils/helpers.py
+++ calculator/utils/helpers.py
@@ -1,9 +1,23 @@
-from typing import Any, Optional
+from typing import Callable, Optional
 
 
-def convert_to_int(x: Any) -> Optional[int]:
-    try:
-        return int(x)
-    except Exception:
-        print(f"Cannot convert input of type {type(x)} to an int")
-        return None
+def timer(fn: Callable) -> Callable:
+    # TODO: Log how much time has passed
+    pass
+
+
+def calculator_logger(fn: Callable) -> Callable:
+    # TODO: Log function name and it's args and kwargs
+    pass
+
+
+def input_parser(fn: Callable) -> Callable:
+    def convert_to_int(*args) -> Optional[int]:
+        # TODO: Enable this function to work with multiple args and kwargs
+        try:
+            return fn(args[0], int(args[1]))
+        except Exception:
+            print(f"Cannot convert input of type {type(args[1])} to an int")
+            return None
+
+    return convert_to_int
